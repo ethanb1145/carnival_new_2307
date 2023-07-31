2fc1dbb7048c2a844e69bae339e9a12aabff442e
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-31 11:49:43 -0600

    Create test for 'most_profitable'

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -66,4 +66,28 @@ RSpec.describe Carnival do
       expect(@carnival.most_popular_ride).to eq(ride2)
     end
   end
+
+  describe "#most_profitable_ride" do 
+    it "gives the most profitable ride of the carnival" do
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      @carnival.add_ride(ride2)
+      visitor1 = Visitor.new('Bruce', 54, '$20')
+      visitor2 = Visitor.new('Tucker', 36, '$20')
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      ride2.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+      ride2.board_rider(visitor1)
+      ride1 = Ride.new({ name: "Carousel", min_height: 24, admission_fee: 1, excitement: :gentle })
+      @carnival.add_ride(ride1)
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor2)
+
+      expect(@carnival.most_profitable_ride).to eq(ride2)
+    end
+  end
 end
