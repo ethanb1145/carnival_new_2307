c6561d43c8354ac0527ef9a8da92b54b19faf4cf
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-31 10:11:04 -0600

    Implement further so that money is taken for revenue for ride

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe Ride do
   end
 
   describe "#board_rider" do 
-    it "adds a visitor to the ride" do 
+    it "adds a visitor to the ride, takes spending money as revenue" do 
       expect(@ride1.rider_log).to eq({})
 
       visitor1 = Visitor.new('Bruce', 54, '$10')
@@ -48,6 +48,9 @@ RSpec.describe Ride do
       @ride1.board_rider(visitor1)
 
       expect(@ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+      expect(visitor1.spending_money).to eq(8)
+      expect(visitor2.spending_money).to eq(4)
+      expect(@ride1.total_revenue).to eq(3)
     end
   end
 end
