8bbd483c84e379c5eff9368dee73a97bbc68557c
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-31 09:58:25 -0600

    Refactor into something else, not sure if this is better or not

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,11 +1,15 @@
 class Visitor
-  attr_reader :name, :height, :spending_money, :preferences
+  attr_reader :name, :height, :spending_money
 
   def initialize(name, height, spending_money)
     @name = name
     @height = height
     @spending_money = spending_money
     @preferences = []
+  end
+
+  def preferences
+    @preferences
   end
 
   def add_preference(preference)
