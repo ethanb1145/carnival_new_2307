074c6f71a8b43d9d7bbb509a785f354cd54f81af
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-31 11:13:39 -0600

    Add duration attribute

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -4,7 +4,14 @@ require './lib/ride'
 
 RSpec.describe Carnival do
   before :each do 
-    @carnival = Carnival.new
+    @carnival = Carnival.new(7)
     expect(@carnival).to be_a(Carnival)
   end
+
+  describe "#initialize" do 
+    it "has a duration" do 
+      expect(@carnival.duration).to eq(7)
+    end
+  end
+
 end
