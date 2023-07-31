bfaef0bb713cb189c45c275910f1a18e9e3a764a
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-31 09:23:01 -0600

    Correct spelling errors

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -2,31 +2,31 @@ require './lib/visitor'
 
 RSpec.describe Visitor do
   before :each do 
-    @vistor1 = Visitor.new("Bruce", 54, "$10")
-    expect(@vistor1).to be_a(Visitor)
+    @visitor1 = Visitor.new("Bruce", 54, "$10")
+    expect(@visitor1).to be_a(Visitor)
   end
 
   describe "#name" do 
     it "gives the name of the visitor" do 
-      expect(@vistor1.name).to eq("Bruce")
+      expect(@visitor1.name).to eq("Bruce")
     end
   end
 
   describe "#height" do 
     it "gives the height of the visitor" do 
-      expect(@vistor1.height).to eq(54)
+      expect(@visitor1.height).to eq(54)
     end
   end
 
   describe "#spending_money" do 
     it "gives the spending money of the visitor" do 
-      expect(@vistor1.spending_money).to eq("$10")
+      expect(@visitor1.spending_money).to eq("$10")
     end
   end
 
   describe "#preferences" do 
     it "gives the preferences of the visitor" do 
-      expect(@vistor1.preferences).to eq([])
+      expect(@visitor1.preferences).to eq([])
 
       @visitor1.add_preference(:gentle)
       @visitor1.add_preference(:thrilling)
