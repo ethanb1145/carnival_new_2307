c68bc012c85173b1b0e49e3984d8845bf61802b5
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-31 11:11:37 -0600

    Begin creating basic class for Carnival

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,1 +1,10 @@
 require './lib/carnival'
+require './lib/visitor'
+require './lib/ride'
+
+RSpec.describe Carnival do
+  before :each do 
+    @carnival = Carnival.new
+    expect(@carnival).to be_a(Carnival)
+  end
+end
